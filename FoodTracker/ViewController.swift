cb6b8ffaf538a5496193ffefc6d132d0b0d951ab
Cory Reed <swashcap@gmail.com>
2017-11-14 00:22:10 -0800

    feat(pkg): wire up UI components to view controller

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -9,11 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Mark: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
 }
 
